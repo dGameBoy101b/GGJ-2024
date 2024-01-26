ae75ace7fc7f378063b3547221bea999d2ef66ae
dGameBoy101b <rhysmader@gmail.com>
2024-01-26 21:39:28 +0800

    Updated fmod parameter script to use property and default its emitter.

--- GGJ-2024/Assets/Scripts/FMOD Parameters/FMODParameter.cs
+++ GGJ-2024/Assets/Scripts/FMOD Parameters/FMODParameter.cs
@@ -1,22 +1,43 @@
 using UnityEngine;
 using FMODUnity;
+using System;
 
 namespace WinterwireGames.FMODParamaters
 {
 	public class FMODParameter : MonoBehaviour
 	{
-		public StudioEventEmitter Emitter;
+		[SerializeField]
+		[Tooltip("The emitter whose parameters this should be linked to.\nDefaults to the first emitter on this object")]
+		private StudioEventEmitter _emitter;
+
+		public StudioEventEmitter Emitter
+		{
+			get
+			{
+				if (this._emitter == null)
+					this._emitter = this.GetComponent<StudioEventEmitter>();
+				return this._emitter;
+			}
+			set => this._emitter = value;
+		}
 
 		public string ParameterName;
 
-		public void SetValue(float value)
+		public float Value
 		{
-			this.Emitter.SetParameter(this.ParameterName, value);
-		}
-
-		private void Awake()
-		{
-			this.Emitter = this.GetComponent<StudioEventEmitter>();
+			get
+			{
+				foreach (var param in this.Emitter.Params)
+				{
+					if (param.Name == this.ParameterName) 
+						return param.Value;
+				}
+				throw new NullReferenceException("No fmod parameter found with name: "+this.ParameterName);
+			}
+			set
+			{
+				this.Emitter.SetParameter(this.ParameterName, value);
+			}
 		}
 	}
 }

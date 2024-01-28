ba450a50e64f18ee984decff87d33c06e3a7075f
MilesJHansen <milesjhansen@gmail.com>
2024-01-28 14:04:08 +0800

    Jester Physics update

--- GGJ-2024/Assets/MotionMatch.cs
+++ GGJ-2024/Assets/MotionMatch.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 
 public class MotionMatch : MonoBehaviour
 {
-    public List<MotionMatchPair> motionMatchPairs;
     public float smoothness;
+    public List<MotionMatchPair> motionMatchPairs;
+    public List<RotationMatchPair> RotationMatchPairs;
 
     [Serializable] public struct MotionMatchPair
     {
         public Transform source;
         public Rigidbody rb;
+    }
+    
+    [Serializable] public struct RotationMatchPair
+    {
+        public Transform source;
+        public Transform target;
     }
     
     private void FixedUpdate()
@@ -21,6 +28,11 @@ public class MotionMatch : MonoBehaviour
             Vector3 targetPosition = Vector3.Lerp(motionMatchPair.rb.position,motionMatchPair.source.position, smoothness );
             
             motionMatchPair.rb.Move(targetPosition, motionMatchPair.source.rotation);
+        }
+
+        foreach (var rotationMatchPair in RotationMatchPairs)
+        {
+            rotationMatchPair.target.localRotation = rotationMatchPair.source.localRotation;
         }
     }
 }
